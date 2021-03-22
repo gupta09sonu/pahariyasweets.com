91cef9303c740a9348bd239b135a4c55e3433764
root <root@ip-172-31-15-201.us-east-2.compute.internal>
2021-03-22 11:16:40 +0000

    shellscripting

---
+++ second.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+: 'kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs
+kdsjfkjs'
+cat << sonu
+my name is sonu gupta i am here to learn shell scripting
+sonu
+
+
+echo "this is my second bash file, permission not attached"
---
+++ third.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cat > example1.txt
